d520e752fb9dd175332476ebc24cdc808d447a92
hami-k <hamidakhatri@gmail.com>
2020-07-29 01:50:29 -0500

    Initial Commit

---
+++ tip_cal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip_cal
+//
+//  Created by Hamida Khatri on 7/29/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
